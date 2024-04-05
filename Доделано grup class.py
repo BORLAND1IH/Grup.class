c0823c34701b0d7a9602eaca90e2edff8679e5f8
Хвостов Иван Евгеньевич <158326488+BORLAND1IH@users.noreply.github.com>
2024-04-05 20:01:05 +0300

    Add files via upload

---
+++ Доделано grup class.py
@@ -0,0 +1,45 @@
+import random
+import time
+
+class Soldier:
+    def __init__(self,number,team):
+        self.number = number
+        self.team = team
+
+    def follow_hero(self,hero):
+        print(f"soldier {self.number} is following Hero {hero.number}")
+
+class Hero:
+    def __init__(self, number):
+        self.number = number
+        self.level = 1
+
+    def increase_level(self):
+        self.level += 1
+
+hero1 = Hero(1)
+hero2 = Hero(2)
+
+soldiers_team1 = []
+soldiers_team2 = []
+
+for i in range(10):
+    soldier = Soldier(i+1, random.choice([1,2]))
+    if soldier.team == 1:
+        soldiers_team1.append(soldier)
+    else:
+        soldiers_team2.append(soldier)
+
+if len(soldiers_team1) > len(soldiers_team2):
+     hero1.increase_level()
+else:
+    hero2.increase_level()
+print("number of soldiers in team 1:", len(soldiers_team1))
+print("number of soldiers in team 2:", len(soldiers_team2))
+
+time.sleep (3)
+
+hero1_soldier = random.choice(soldiers_team1)
+hero1_soldier.follow_hero(hero1)
+
+print(f"hero {hero1.number} and Soldier {hero1_soldier.number}")
